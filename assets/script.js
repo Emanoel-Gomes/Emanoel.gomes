c50b99990d4dd0b03ffa3acd138d3e08c102ac13
Emanoel-Gomes <emanoel.gomes@icen.ufpa.br>
2023-12-11 12:22:28 -0300

    criação do campo de educação e experiências

--- assets/script.js
+++ assets/script.js
@@ -162,3 +162,23 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+
+const tabs = document.querySelectorAll("[data-target]");
+  const tabContents = document.querySelectorAll("[data-content]");
+
+  tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+      const target = document.querySelector(tab.dataset.target);
+
+      tabContents.forEach(tabContent => {
+        tabContent.classList.remove("qualification__active");
+      });
+      tabs.forEach(tab => {
+        tab.classList.remove("qualification__active");
+      });
+
+      tab.classList.add("qualification__active");
+      target.classList.add("qualification__active");
+    });
+  });
